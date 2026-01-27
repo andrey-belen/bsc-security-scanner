1d0ae3cdcbb49c7c3e3b77fe80e7fb031e01d0a7
Andrei Belenkii <andreybelenkiy77@gmail.com>
2026-01-27 16:35:13 +0700

    Use SQLite in production

--- knexfile.js
+++ knexfile.js
@@ -22,18 +22,11 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT || 5432,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+    client: 'better-sqlite3',
+    connection: {
+      filename: './database/bsc_scanner.db'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'knex_migrations'
